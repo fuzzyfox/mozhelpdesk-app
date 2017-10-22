64dcb62df2d43864d2bad662d8e84ff8d11edfdd
William Duyck <fuzzyfox0@gmail.com>
2017-10-23 00:02:19 +0100

    📝 Improve docs in ticket vuex module

--- src/store/modules/tickets.js
+++ src/store/modules/tickets.js
@@ -8,6 +8,12 @@ export const initState = () => {
 export const state = initState()
 
 export const mutations = {
+  /**
+   * Commit a ticket to the store
+   *
+   * @param {object} state    Vuex module state
+   * @param {object} ticket   Ticket (or ticket fragment) to save
+   */
   [mutationType.SAVE_TICKET](state, ticket) {
     const existingTicket = state[ticket._id] || {}
 
@@ -19,24 +25,54 @@ export const mutations = {
   }
 }
 
+// NOTE: Getters are documented as they are used, not how they are constructed.
 export const getters = {
-  getTicketsAsArray(state) {
+  /**
+   * Tickets as an array
+   *
+   * @type {Array.<object>}
+   */
+  ticketsAsArray(state) {
     return Object.values(state)
   },
+  /**
+   * Get ticket by status
+   *
+   * @param  {(string|Array.<string>)} status status(es) to filter tickets by
+   * @return {Array.<object>}                 Filtered collection of tickets
+   */
   getTicketsByStatus(state, getters) {
     return status =>
-      getters.getTicketsAsArray.filter(
-        ticket => ticket.mozhelp_status === status.toUpperCase()
+      getters.ticketsAsArray.filter(
+        ticket =>
+          Array.isArray(status)
+            ? status.includes(ticket.mozhelp_status)
+            : ticket.mozhelp_status === status.toUpperCase()
       )
   },
+  /**
+   * Get ticket by ID
+   *
+   * @param  {string} ticketId Ticket helpdesk ID
+   * @return {object}          Ticket
+   */
   getTicketById(state) {
     return ticketId => state[ticketId]
   }
 }
 
 export const actions = {
-  saveTicket({ commit }, ticket) {
+  /**
+   * Save a ticket to the store
+   *
+   * @param  {object}           context Vuex module context
+   * @param  {object}           ticket  Ticket (or ticket fragment) to save to the store
+   * @return {Promise.<object>}         Promise of the stored ticket
+   */
+  saveTicket({ state, commit }, ticket) {
     commit(mutationType.SAVE_TICKET, ticket)
+
+    return Promise.resolve(state[ticket._id])
   }
 }
 
